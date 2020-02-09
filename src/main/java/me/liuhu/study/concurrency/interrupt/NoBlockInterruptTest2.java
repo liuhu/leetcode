73995893905ba55c35270cffb8cd1e4e53c895de
liuhu <mail.liuhu@gmail.com>
2020-02-09 17:03:50 +0800

    线程中断测试

--- src/main/java/me/liuhu/study/concurrency/interrupt/NoBlockInterruptTest2.java
+++ src/main/java/me/liuhu/study/concurrency/interrupt/NoBlockInterruptTest2.java
@@ -5,22 +5,24 @@ package me.liuhu.study.concurrency.interrupt;
  * @author: LiuHu
  * @create: 2020/2/9
  **/
-public class Test1 implements Runnable {
+public class NoBlockInterruptTest2 implements Runnable {
     @Override
     public void run() {
         for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            // 如果线程被中断，则不再执行
+            if (Thread.currentThread().isInterrupted()) {
+                return;
+            }
             if (i % 10000 == 0) {
                 System.out.println("10000的整数: " + i);
             }
         }
     }
 
-    public static void main(String[] args) {
-        Thread thread = new Thread(new Test1());
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(new NoBlockInterruptTest2());
         thread.start();
-        // interrupt 仅仅是一个中断信号
-        // 具体的如何操作需要被中断的线程自己处理
-        // 当前的例子，子线程没有任何处理，所以会一直运行到计算完毕
+        Thread.sleep(100);
         thread.interrupt();
     }
 }
