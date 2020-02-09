af39dab343aa18b9b388229ae36b79404b535415
liuhu <mail.liuhu@gmail.com>
2020-02-09 17:00:57 +0800

    线程中断测试

---
+++ src/main/java/me/liuhu/study/concurrency/interrupt/Test1.java
@@ -0,0 +1,26 @@
+package me.liuhu.study.concurrency.interrupt;
+
+/**
+ * @description:
+ * @author: LiuHu
+ * @create: 2020/2/9
+ **/
+public class Test1 implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            if (i % 10000 == 0) {
+                System.out.println("10000的整数: " + i);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(new Test1());
+        thread.start();
+        // interrupt 仅仅是一个中断信号
+        // 具体的如何操作需要被中断的线程自己处理
+        // 当前的例子，子线程没有任何处理，所以会一直运行到计算完毕
+        thread.interrupt();
+    }
+}
