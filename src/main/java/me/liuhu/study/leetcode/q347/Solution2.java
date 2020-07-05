931ab40aa0222127e347b0fcd00e02583f7530da
liuhu <mail.liuhu@gmail.com>
2020-07-05 23:05:59 +0800

    前 K 个高频元素

--- src/main/java/me/liuhu/study/leetcode/q347/Solution2.java
+++ src/main/java/me/liuhu/study/leetcode/q347/Solution2.java
@@ -1,14 +1,15 @@
 package me.liuhu.study.leetcode.q347;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
 
 /**
  * @description:
  * @author: LiuHu
  * @create: 2020/7/5
  **/
-public class Solution1 implements Solution {
+public class Solution2 implements Solution {
     @Override
     public int[] topKFrequent(int[] nums, int k) {
         int[] result = new int[k];
@@ -18,12 +19,10 @@ public class Solution1 implements Solution {
             map.compute(num, (_k, _v) -> null == _v ? 1 : _v + 1);
         }
 
-        List<Map.Entry<Integer, Integer>> res = map.entrySet().stream()
-                .sorted(Comparator.comparingInt(Map.Entry::getValue))
-                .collect(Collectors.toList());
-
+        PriorityQueue<Map.Entry<Integer, Integer>> queue = new PriorityQueue<>((v1, v2) -> v2.getValue() - v1.getValue());
+        queue.addAll(map.entrySet());
         for (int i = 0; i < k; i++) {
-            result[i] = res.get(res.size() - i - 1).getKey();
+            result[i] = queue.poll().getKey();
         }
         return result;
     }
