be3bda01fa8b6a864eaa9598252586f4bef11338
liuhu <mail.liuhu@gmail.com>
2020-07-06 21:10:30 +0800

    爬楼梯 递归

--- src/main/java/me/liuhu/study/leetcode/q70/Solution1.java
+++ src/main/java/me/liuhu/study/leetcode/q70/Solution1.java
@@ -9,21 +9,25 @@ import java.util.Map;
  * @create: 2020/7/6
  **/
 public class Solution1 implements Solution {
+    Map<Integer, Integer> cache = new HashMap<>();
 
     @Override
     public int climbStairs(int n) {
-        Map<Integer, Integer> cache = new HashMap<>();
         if (n == 1) {
             return 1;
         }
         if (n == 2) {
             return 2;
         }
-        int _1 = climbStairs(n - 1);
-        int _2 = climbStairs(n - 2);
-        cache.putIfAbsent(n - 1, _1);
-        cache.putIfAbsent(n - 2, _2);
-
+        int _1, _2;
+        if (null == cache.get(n - 1)) {
+            _1 = climbStairs(n - 1);
+            cache.put(n - 1, _1);
+        }
+        if (null == cache.get(n - 2)) {
+            _2 = climbStairs(n - 2);
+            cache.put(n - 2, _2);
+        }
         return cache.get(n - 1) + cache.get(n - 2);
     }
 }
