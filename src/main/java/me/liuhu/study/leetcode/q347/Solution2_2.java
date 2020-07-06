9f8bd7a9fe33b12eaba24b78f4d1dcdc4772f4d1
liuhu <mail.liuhu@gmail.com>
2020-07-06 11:14:36 +0800

    Top k 2 刷

---
+++ src/main/java/me/liuhu/study/leetcode/q347/Solution2_2.java
@@ -0,0 +1,33 @@
+package me.liuhu.study.leetcode.q347;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+/**
+ * @description:
+ * @author: LiuHu
+ * @create: 2020/7/6
+ **/
+public class Solution2_2 implements Solution {
+
+    @Override
+    public int[] topKFrequent(int[] nums, int k) {
+
+        int[] res = new int[k];
+        Map<Integer, Integer> map = new HashMap<>(nums.length);
+
+        for(int num : nums) {
+            map.compute(num, (_k, _v) -> null == _v ? 1 : _v + 1);
+        }
+
+        PriorityQueue<Map.Entry<Integer, Integer>> queue = new PriorityQueue<>((v1, v2) -> v2.getValue() - v1.getValue());
+        queue.addAll(map.entrySet());
+
+        for (int i = 0; i < k; i++) {
+            res[i] = queue.poll().getKey();
+        }
+
+        return res;
+    }
+}
