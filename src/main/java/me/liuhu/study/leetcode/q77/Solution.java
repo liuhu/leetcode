c847e6d4fd4f1de57704fe3137f6465e9c260116
liuhu <mail.liuhu@gmail.com>
2020-07-14 18:14:59 +0800

    77. 组合， 回溯（dfs）解法

---
+++ src/main/java/me/liuhu/study/leetcode/q77/Solution.java
@@ -0,0 +1,23 @@
+package me.liuhu.study.leetcode.q77;
+
+import java.util.List;
+
+/**
+ * 给定两个整数 n 和 k，返回 1 ... n 中所有可能的 k 个数的组合。
+ * 输入: n = 4, k = 2
+ * 输出:
+ * [
+ *   [1,2],
+ *   [1,3],
+ *   [1,4],
+ *   [2,3],
+ *   [2,4],
+ *   [3,4]
+ * ]
+ * @description:
+ * @author: LiuHu
+ * @create: 2020/7/14
+ **/
+public interface Solution {
+    List<List<Integer>> combine(int n, int k);
+}
---
+++ src/main/java/me/liuhu/study/leetcode/q77/Solution1.java
@@ -0,0 +1,56 @@
+package me.liuhu.study.leetcode.q77;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/**
+ * @description:
+ * @author: LiuHu
+ * @create: 2020/7/14
+ **/
+public class Solution1 implements Solution {
+
+    @Override
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (k > n) {
+            return result;
+        }
+        if (n <= 0) {
+            return result;
+        }
+        List<Integer> nums = IntStream.range(1, n + 1).boxed().collect(Collectors.toList());
+
+        dfs(new LinkedList<>(), nums, result, 0, k);
+
+        return result;
+    }
+
+    public void dfs(LinkedList<Integer> path, List<Integer> nums, List<List<Integer>> result, int level, int k) {
+        if (k == level) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = level; i < nums.size() ; i++) {
+            if (null != path && path.size() > 0 && path.getLast() >= nums.get(i)) {
+                continue;
+            }
+            path.addLast(nums.get(i));
+            dfs(path, nums, result, level + 1, k);
+
+            // 回溯
+            path.removeLast();
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution1();
+        List<List<Integer>> result = solution.combine(4,2);
+        System.out.println(result);
+    }
+}
