b34f30bb318530a5a6e51d708da09645080e8e71
liuhu <mail.liuhu@gmail.com>
2020-07-16 15:20:35 +0800

    后序遍历 5刷

---
+++ src/main/java/me/liuhu/study/leetcode/q145/Solution2_5.java
@@ -0,0 +1,37 @@
+package me.liuhu.study.leetcode.q145;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * @description:
+ * @author: LiuHu
+ * @create: 2020/7/16
+ **/
+public class Solution2_5 implements Solution {
+
+    @Override
+    public List<Integer> postorderTraversal(TreeNode root) {
+        LinkedList<Integer> result = new LinkedList<>();
+        if (null == root) {
+            return result;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            result.addFirst(node.val);
+            if (null != node.left) {
+                stack.push(node.left);
+            }
+            if (null != node.right) {
+                stack.push(node.right);
+            }
+        }
+
+        return result;
+    }
+}
