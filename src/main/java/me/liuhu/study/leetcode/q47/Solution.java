3ae96565d1fd7a750f680ede2e910d3f090a5e4c
liuhu <mail.liuhu@gmail.com>
2020-07-16 17:29:04 +0800

    47. 全排列 II， 1刷

---
+++ src/main/java/me/liuhu/study/leetcode/q47/Solution.java
@@ -0,0 +1,22 @@
+package me.liuhu.study.leetcode.q47;
+
+import java.util.List;
+
+/**
+ * 47. 全排列 II
+ * 输入: [1,1,2]
+ * 输出:
+ * [
+ *   [1,1,2],
+ *   [1,2,1],
+ *   [2,1,1]
+ * ]
+ *
+ * @description:
+ * @author: LiuHu
+ * @create: 2020/7/16
+ **/
+public interface Solution {
+
+    List<List<Integer>> permuteUnique(int[] nums);
+}
---
+++ src/main/java/me/liuhu/study/leetcode/q47/Solution1.java
@@ -0,0 +1,53 @@
+package me.liuhu.study.leetcode.q47;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @description:
+ * @author: LiuHu
+ * @create: 2020/7/16
+ **/
+public class Solution1 implements Solution {
+
+    @Override
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        int len = nums.length;
+        if (len == 0) {
+            return new ArrayList<>();
+        }
+        boolean[] used = new boolean[len];
+        Set<List<Integer>> result = new HashSet<>();
+        dfs(result, new ArrayList<>(), used, nums, 0, len);
+        return new ArrayList<>(result);
+    }
+
+    private void dfs(Set<List<Integer>> result, List<Integer> path, boolean[] used, int[] nums, int level, int len) {
+        if (len == path.size()) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = 0; i < len ; i++) {
+            if (!used[i]) {
+                path.add(nums[i]);
+                used[i] = true;
+
+                dfs(result, path, used, nums, level + 1, len);
+
+                path.remove(path.size() - 1);
+                used[i] = false;
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Solution solution = new Solution1();
+        int[] nums = new int[]{1, 1, 2};
+        List<List<Integer>> lists = solution.permuteUnique(nums);
+        System.out.println(lists);
+    }
+}
