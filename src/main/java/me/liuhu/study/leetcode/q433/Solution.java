18f74b3ecdaccf2635d263dba34de3945e3c0b75
liuhu <mail.liuhu@gmail.com>
2020-07-26 10:59:33 +0800

    433. 最小基因变化

--- src/main/java/me/liuhu/study/leetcode/q433/Solution.java
+++ src/main/java/me/liuhu/study/leetcode/q433/Solution.java
@@ -1,6 +1,14 @@
 package me.liuhu.study.leetcode.q433;
 
 /**
+ * 433. 最小基因变化
+ * https://leetcode-cn.com/problems/minimum-genetic-mutation/
+ *
+ * start: "AACCGGTT"
+ * end:   "AAACGGTA"
+ * bank: ["AACCGGTA", "AACCGCTA", "AAACGGTA"]
+ * 返回值: 2
+ *
  * @description:
  * @author: LiuHu
  * @create: 2020/7/22
