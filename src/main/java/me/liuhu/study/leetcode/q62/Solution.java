cdae46fce8430254e89eacbff230417eb5a863c5
liuhu <mail.liuhu@gmail.com>
2020-08-03 22:37:08 +0800

    62. 不同路径 1刷

---
+++ src/main/java/me/liuhu/study/leetcode/q62/Solution.java
@@ -0,0 +1,17 @@
+package me.liuhu.study.leetcode.q62;
+
+/**
+ * 62. 不同路径
+ * https://leetcode-cn.com/problems/unique-paths/
+ *
+ * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
+ * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
+ * 问总共有多少条不同的路径？
+ *
+ * @description:
+ * @author: LiuHu
+ * @create: 2020/8/3
+ **/
+public interface Solution {
+    int uniquePaths(int m, int n);
+}
